13f833fe669742236fe294994743434a315090a0
Philip (flip) Kromer <flip@infochimps.org>
2011-01-22 00:22:43 -0600

    jruby => hellow world

--- src/dr_tutorial/pt1_hello_with_java/runner.sh
+++ src/dr_tutorial/pt1_hello_with_java/runner.sh
@@ -6,5 +6,9 @@ DATARUSH_PATH=$DR_WUKONG_PATH/../datarush
 
 # echo $DR_TUTORIAL_PATH $DR_WUKONG_PATH $DATARUSH_PATH
 
-javac                 -cp $DATARUSH_PATH/lib/datarush-library-5.0.0-27.jar:$DATARUSH_PATH/lib/*.jar:$DR_TUTORIAL_PATH $DR_TUTORIAL_PATH/*.java
-$DATARUSH_PATH/bin/dr -cp $DATARUSH_PATH/lib/datarush-library-5.0.0-27.jar:$DATARUSH_PATH/lib/*.jar:$DR_TUTORIAL_PATH HelloWorld
+
+CLASSPATH="$CLASSPATH:$DATARUSH_PATH/lib/datarush-library-5.0.0-27.jar:$DATARUSH_PATH/lib/*.jar:$DR_TUTORIAL_PATH"
+CLASSPATH=$CLASSPATH:/usr/local/share/jruby/lib/jruby.jar:/usr/local/share/jruby/lib/jruby.jar
+
+javac                 -cp $CLASSPATH $DR_TUTORIAL_PATH/*.java
+$DATARUSH_PATH/bin/dr -cp $CLASSPATH HelloWorld
--- src/dr_tutorial/pt2_hello_with_jruby/HelloWorld.java
+++ src/dr_tutorial/pt2_hello_with_jruby/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
   }
 
   public static void main(String[] args) {
-    new ProduceHelloWorld();
+    new HelloWorld();
   }
 }
 
